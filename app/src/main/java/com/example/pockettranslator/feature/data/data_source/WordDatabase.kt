c51878b8ac2144c135266bba38d0aa44e0bc4bff
Maksim Zozuliak <max1max2mega56@gmail.com>
2022-12-15 13:12:42 +0200

    v 1

--- app/src/main/java/com/example/pockettranslator/feature/data/data_source/WordDatabase.kt
+++ app/src/main/java/com/example/pockettranslator/feature/data/data_source/WordDatabase.kt
@@ -4,12 +4,14 @@ import android.annotation.SuppressLint
 import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.pockettranslator.feature.domain.model.Word
 
 @Database(
     entities = [Word::class],
     version = 1
 )
+@TypeConverters(DataConverter::class)
 abstract class WordDatabase: RoomDatabase() {
 
     abstract val wordDao: WordDao
